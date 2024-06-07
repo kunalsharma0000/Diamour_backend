ae048b2dd584b33d8d58d1a2931389c1857008df
kunalsharma0000 <kunalsharma999010@gmail.com>
2024-06-07 13:14:36 +0530

    changes hai

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 
-python5.0.6 -m pip install -r requirements.txt
-python5.0.6 manage.py collectstatic  --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic  --noinput --clear
 
 echo "BUILD END"
