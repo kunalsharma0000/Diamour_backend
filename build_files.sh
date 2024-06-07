a814d053f35b4db51f0ef019a5af651209d52a13
kunalsharma0000 <kunalsharma999010@gmail.com>
2024-06-07 13:19:13 +0530

    commit hai

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-echo "BUILD START"
 
-python -m pip install -r requirements.txt
-python manage.py collectstatic  --noinput --clear
+ -m pip install -r requirements.txt
+python3.9 manage.py collectstatic  --noinput --clear
 
-echo "BUILD END"
