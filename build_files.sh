b22f0d636f240a5af2cc3c7d28dc49ad98a0ec11
kunalsharma0000 <kunalsharma999010@gmail.com>
2024-06-10 14:23:01 +0530

    version

--- build_files.sh
+++ build_files.sh
@@ -3,12 +3,12 @@
 #build the project
 
 echo "Building the project"
-python5 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration "
-python5 manage.py makemigration --noinput
-python5 manage.py migrate --noinput
+python3.9 manage.py makemigration --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static"
 
-python5 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
