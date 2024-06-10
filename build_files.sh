47b353ad5e26a4b61234639a5edd6325c4ee628f
kunalsharma0000 <kunalsharma999010@gmail.com>
2024-06-10 13:47:32 +0530

    changes jai

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,14 @@
 
 #!/bin/bash
-source /path/to/your/venv/bin/activate
-python manage.py collectstatic --noinput
+#build the project
 
+echo "Building the project"
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration "
+python3.9 manage.py makemigration --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static"
+
+python3.9 manage.py collectstatic --noinput --clear
